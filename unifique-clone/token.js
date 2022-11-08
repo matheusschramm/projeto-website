29b1ba6e1b4c53e4ebebd1d55d651bc67f85a44b
matheus.schramm <matheuscramm147@gmail.com>
2022-11-08 16:47:49 -0300

    OK

---
+++ unifique-clone/token.js
@@ -0,0 +1,10 @@
+
+        function getUrlBase(port) {
+            return "https://aubwytcpmbmvdtzlsogr.supabase.co/rest/v1/" + port+ "select=*";
+         }
+
+ function getTokenSupabase() {
+             return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF1Ynd5dGNwbWJtdmR0emxzb2dyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjMwMDEzNzEsImV4cCI6MTk3ODU3NzM3MX0.0_Cf5TH_00raNHLasOl5ZK2mVvkygf3ZBn8iFm1HHrI";
+
+             // return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZkY3N6cXZ2cndkcWNuanZjb3h0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyNTUxNTUsImV4cCI6MTk3NTgzMTE1NX0.U-3HSFgKo9ydTnKrpQsx5ytrBcLSpGwzVn6LqNwn14E";
+         }
